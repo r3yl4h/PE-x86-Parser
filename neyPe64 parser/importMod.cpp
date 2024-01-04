11fa2b9a76da1adf1a36da05da7fab6389ba565a
yunamari <151783331+yunamari@users.noreply.github.com>
2024-01-04 22:11:22 +0100

    Update importMod.cpp for print the import table

--- neyPe64 parser/importMod.cpp
+++ neyPe64 parser/importMod.cpp
@@ -3,7 +3,14 @@
 #include <iomanip>
 #include "peh.hpp"
 
-void idata(HANDLE hFile) {
+void idata(const CHAR* path) {
+    bool addrid = true;
+    HANDLE hFile = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+    if (hFile == INVALID_HANDLE_VALUE){
+        CloseHandle(hFile);
+        std::cout << "the file does not exist or we were unable to open it" << std::endl;
+    }
     DWORD fileSize = GetFileSize(hFile, nullptr);
     BYTE* fileContent = new BYTE[fileSize];
     DWORD bytesRead;
@@ -24,7 +31,6 @@ void idata(HANDLE hFile) {
 
     if (importTable->VirtualAddress != 0 && importTable->Size != 0) {
         PIMAGE_SECTION_HEADER sectionHeader = IMAGE_FIRST_SECTION(ntHeaders);
-        std::cout << "\nImport Table Address: 0x" << std::hex << importTable->VirtualAddress + optHeader64->ImageBase << std::dec << std::endl;
 
         for (int i = 0; i < ntHeaders->FileHeader.NumberOfSections; ++i) {
             if (importTable->VirtualAddress >= sectionHeader[i].VirtualAddress && importTable->VirtualAddress < sectionHeader[i].VirtualAddress + sectionHeader[i].SizeOfRawData) {
@@ -33,7 +39,6 @@ void idata(HANDLE hFile) {
                         fileContent + importTable->VirtualAddress - sectionHeader[i].VirtualAddress +
                         sectionHeader[i].PointerToRawData);
 
-                std::cout << "Import Table:\n";
                 while (importDescriptor->Name != 0) {
                     const char* dllName = reinterpret_cast<const char*>(fileContent + importDescriptor->Name - sectionHeader[i].VirtualAddress + sectionHeader[i].PointerToRawData);
                     auto thunkData = reinterpret_cast<PIMAGE_THUNK_DATA64>(fileContent + importDescriptor->OriginalFirstThunk - sectionHeader[i].VirtualAddress + sectionHeader[i].PointerToRawData);
@@ -45,8 +50,17 @@ void idata(HANDLE hFile) {
                             auto importByName = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(fileContent + thunkData->u1.AddressOfData - sectionHeader[i].VirtualAddress + sectionHeader[i].PointerToRawData);
                             funcName = importByName->Name;
                         }
+                        auto funcAddr = thunkData->u1.Function + optHeader64->ImageBase;
 
-                        std::cout << "\tAddress: 0x" << std::hex << thunkData->u1.Function + optHeader64->ImageBase << std::dec << std::setw(20)
+                        if(addrid){
+                            auto baseIdata = funcAddr & 0xFFFFF000;
+                            std::cout << "\nImport Table Address: 0x" << std::hex << std::setw(8) << std::setfill('0') << baseIdata << std::dec << std::endl;
+                            std::cout << std::dec << std::setw(0) << std::setfill(' ');
+                            std::cout << "Import Table:\n";
+                            addrid = false;
+                        }
+
+                        std::cout << "\tAddress: 0x" << std::hex << funcAddr << std::dec << std::setw(20)
                                   << "\tDLL: " << dllName << std::setw(20) << " \tFunction: " << (funcName ? funcName : "Ordinal") << std::setw(20) << std::endl;
 
                         ++thunkData;
